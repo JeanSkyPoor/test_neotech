08e6bf4adeedd26701453031241184dc19ca3f38
Jean_Sky_Poor <katovano2705@gmail.com>
2023-12-07 23:00:43 +0300

    add second graph

--- main.py
+++ main.py
@@ -13,11 +13,17 @@ Reading data for graphs. Please, wait
 )
 def read_total_data():
     df_registration_dynamic_by_country = read_registration_dynamic_by_country()
+    df_first_deposits_dynamic_by_country = read_first_deposits_dynamic_by_country()
 
-    return df_registration_dynamic_by_country
+    returned_tuple = (
+        df_registration_dynamic_by_country,
+        df_first_deposits_dynamic_by_country
+    )
+
+    return returned_tuple
 
 
-df_registration_dynamic_by_country = read_total_data()
+total_data = read_total_data()
 
 
 def draw_graph_registration_dynamic_by_country(
@@ -47,6 +53,33 @@ def draw_graph_registration_dynamic_by_country(
     )
 
 
+def draw_graph_first_deposits_dynamic_by_country(
+        df_first_deposits_dynamic_by_country: pd.DataFrame
+) -> None:
+    fig = px.line(
+        df_first_deposits_dynamic_by_country,
+        x = "date",
+        y = "first_deposit_count",
+        color = "country",
+        markers = True,
+        symbol = "country",
+        title = "Dynamics of first deposits by country"
+    )
+
+    fig.update_xaxes(
+        title_text = "Date"
+    )
+
+    fig.update_yaxes(
+        title_text = "First deposits count"
+    )
+    
+    st.plotly_chart(
+        fig,
+        use_container_width = True
+    )
+
+
 ### FIRST ###
 
 graph, table = st.tabs(
@@ -56,10 +89,10 @@ graph, table = st.tabs(
     ]
 )
 with graph:
-    draw_graph_registration_dynamic_by_country(df_registration_dynamic_by_country)
+    draw_graph_registration_dynamic_by_country(total_data[0])
 with table:
     st.dataframe(
-        df_registration_dynamic_by_country,
+        total_data[0],
         use_container_width = True,
         hide_index = True,
         column_config = {
@@ -69,3 +102,23 @@ with table:
 st.divider()
 
 
+### SECOND ###
+
+graph, table = st.tabs(
+    [
+        "Graph",
+        "Table"
+    ]
+)
+with graph:
+    draw_graph_first_deposits_dynamic_by_country(total_data[1])
+with table:
+    st.dataframe(
+        total_data[1],
+        use_container_width = True,
+        hide_index = True,
+        column_config = {
+            "date": st.column_config.DateColumn()
+        }
+    )
+st.divider()
--- secondary_defs.py
+++ secondary_defs.py
@@ -19,3 +19,23 @@ def read_registration_dynamic_by_country() -> pd.DataFrame:
     )
 
     return df_registration_dynamic_by_country
+
+
+def read_first_deposits_dynamic_by_country() -> pd.DataFrame:
+    df_columns = {
+        "country": "category",
+        "date": str,
+        "first_deposit_count": float
+    }
+
+    df_first_deposits_dynamic_by_country = pd.read_csv(
+        "first_deposits_dynamic_by_country.csv",
+        usecols = df_columns.keys(),
+        dtype = df_columns,
+        sep = ",",
+        parse_dates = [
+            "date"
+        ]
+    )
+
+    return df_first_deposits_dynamic_by_country
